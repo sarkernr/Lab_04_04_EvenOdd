462990a472cc374290eb59abd374b98c558fd875
naomi <sarkernr@mail.uc.edu>
2024-09-24 18:52:10 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int number1 = 26;
+        int number2 = 71;
+
+        if(number1 % 2 == 0){
+            System.out.println(number1 + " is even");}
+        else{
+            System.out.println(number1 + " is odd");}
+        if(number2 % 2 == 0){
+            System.out.println(number2 + " is even");}
+        else{
+            System.out.println(number2 + " is odd");}
+        }
+    }
